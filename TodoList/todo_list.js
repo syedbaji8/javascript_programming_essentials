5453528be70e53dcde9d61ef6a42feac7cae2b2c
syedbaji8 <syedbaji8@gmail.com>
2026-07-31 08:38:26 -0400

    init commit

---
+++ TodoList/todo_list.js
@@ -0,0 +1,9 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+
+const tasks = [];
+function addTask(){
+
+}
