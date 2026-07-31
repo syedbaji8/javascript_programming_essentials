f395c953b31bfec22de8949ed6c87e57d50c3f69
syedbaji8 <syedbaji8@gmail.com>
2026-07-31 12:51:21 -0400

    changes added

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -4,6 +4,17 @@ const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
 
 const tasks = [];
-function addTask(){
-
+function addTask() {
+    const user_input_value = taskInput.value;
+    tasks.push({text: user_input_value, completed: false})
+    displayTasks()
+}
+addTaskBtn.addEventListener('click', addTask);
+function displayTasks() {
+    const rendertasks = tasks.map((task, index) => {
+        return `<li><input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+        <label for="task-${index}">${task.text}</label></li>`;
+        
+    }).join('');
+    taskList.innerHTML = rendertasks;
 }
