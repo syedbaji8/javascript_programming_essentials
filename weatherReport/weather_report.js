b1d4e1f11807963086aa771f730fa66c03403e13
syedbaji8 <syedbaji8@gmail.com>
2026-08-03 07:27:56 -0400

    try catch method implemented

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -12,7 +12,22 @@ function showweatherDetails(event) {
     event.preventDefault();
     const userCityInputValue = document.getElementById('city');
     try{
-        
+        if(!userCityInputValue.value){
+            throw new Error('Please fill the input with proper city name')
+        }
+        if(userCityInputValue.value === ''){
+            throw new Error('Please fill the input with proper city name')
+        }
+        fetchWeatherFromApi(userCityInputValue.value)
+        .then((data) => {
+            console.log(data);
+            const renderHtml = `<h2>Weather in ${data.name}</h2>
+            <p>Temperature: ${data.main?.temp}</p>
+            <p>Temperature: ${data.weather?.[0]?.description || 'N/A'}</p>`
+            weatherInfo.innerHTML = renderHtml;
+        })
+    }catch(error){
+        console.error(error)
     }
 }
 weatherForm.addEventListener('submit', showweatherDetails);
