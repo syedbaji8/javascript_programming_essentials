3fdc3b6463ab84d9562324b77ca7cce2088c8c9b
syedbaji8 <syedbaji8@gmail.com>
2026-08-03 06:24:58 -0400

    init commit

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,9 @@
+let url = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
+
+function fetchCurrentWeatherReport(){
+    const userCityInput = document.getElementById('city');
+
+}
+function fetchWeatherFromApi(city){
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Delhi,07,91&limit=7&appid=57f99d0e602b03aa136057c13262f12757f99d0e602b03aa136057c13262f127`)
+}
