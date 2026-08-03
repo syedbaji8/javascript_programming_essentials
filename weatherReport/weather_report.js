68e432a8ec18cab00a19728dbf27175253c8e3dd
syedbaji8 <syedbaji8@gmail.com>
2026-08-03 07:20:50 -0400

    weather data pulled

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,9 +1,18 @@
-let url = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
+const apiKey = '57f99d0e602b03aa136057c13262f127';
 
-function fetchCurrentWeatherReport(){
-    const userCityInput = document.getElementById('city');
+const weatherForm = document.getElementById('weatherForm');
+const weatherInfo = document.getElementById('weatherInfo');
 
+async function fetchWeatherFromApi(city) {
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`, { method: 'GET' });
+    const data = await result.json();
+    return data;
 }
-function fetchWeatherFromApi(city){
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Delhi,07,91&limit=7&appid=57f99d0e602b03aa136057c13262f12757f99d0e602b03aa136057c13262f127`)
+function showweatherDetails(event) {
+    event.preventDefault();
+    const userCityInputValue = document.getElementById('city');
+    try{
+        
+    }
 }
+weatherForm.addEventListener('submit', showweatherDetails);
